7004653eedb141b0ff7be0fb67881d98e7c90ea5
mkylmz <kaan.yilmaz@metu.edu.tr>
2021-05-28 21:24:31 +0300

    Add larger step size test for slip2d

--- slip/test_slip.py
+++ slip/test_slip.py
@@ -3,9 +3,17 @@ from slip2d import slip2d
 import numpy as np
 import matplotlib.pyplot as plt
 
-slip = slip2d([0, 0.80, 0, 0, 0, 0.51], 0, 0.29, 1/240)
+slip = slip2d([0, 0.30, 0, 0, 0, 0], 0, 0.29, 1/240)
 
-sol = slip.step_apex_to_apex()
+last_time = 0
 
-plt.plot(sol.t,sol.y[1], color='orange')
+for i in range(1,10):
+
+    sol = slip.step_apex_to_apex()
+    sol.t = last_time + sol.t
+    last_time = sol.t[-1]
+
+    plt.plot(sol.t,sol.y[1], color='orange')
+    slip.update_state(sol.y[0][-1],sol.y[1][-1],sol.y[2][-1],sol.y[3][-1])
+
 plt.show()
