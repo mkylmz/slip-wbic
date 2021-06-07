db7c6e2dec505b3aae7596a04a7cd7db147bd75a
mkylmz <kaan.yilmaz@metu.edu.tr>
2021-06-07 22:29:35 +0300

    Add energy injection example

--- slip/test_slip_w_controller.py
+++ slip/test_slip_w_controller.py
@@ -4,22 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-rest_length = 0.29
+rest_length = 0.27
 slip = slip2d([0, 0.32, 0, 0, 0, 0], 0, rest_length, 1/240)
 
 last_time = 0
 sol = slip.step_apex_to_apex()
 plt.plot(sol.y[0],sol.y[1], color='orange')
-K_p = 0.05
-xdot_des = 0.1
+K_p = 0.01
+xdot_des = 0.5
+slip.set_target_vel(xdot_des)
 
-for i in range(1,8):
+for i in range(1,30):
+
 
     # Update time variables
     sol.t = last_time + sol.t
     last_time = sol.t[-1]
     ## Plot Results
     plt.plot(sol.y[0],sol.y[1], color='orange')
+    if sol.failed:
+        break
     # Update new state variables
     slip.update_state(sol.y[0][-1],sol.y[1][-1],sol.y[2][-1],sol.y[3][-1])
     ## Use Raiberts controller
@@ -33,5 +37,5 @@ for i in range(1,8):
     ## Use slip model apex to apex
     sol = slip.step_apex_to_apex()
 
-
+print(i," timesteps could be calculated")
 plt.show()
