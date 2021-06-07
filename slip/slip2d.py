3ad546fc04149c557f5392480396aa03d3b1c8b9
mkylmz <kaan.yilmaz@metu.edu.tr>
2021-06-07 22:29:24 +0300

    Add energy injection to slip

--- slip/slip2d.py
+++ slip/slip2d.py
@@ -15,10 +15,11 @@ class slip2d():
         self.dt         = dt
 
         self.mass       = 11.00
-        self.stiffness  = 8000.0
+        self.STIFFNESS  = 30000.0
         self.r_length   = rest_length
         self.cur_length = rest_length
         self.aoa        = aoa
+        self.target_vel = 0
 
         self.state[4]   = self.state[0]+np.sin(self.aoa)*rest_length
         self.state[5]   = self.state[1]-np.cos(self.aoa)*rest_length
@@ -37,10 +38,10 @@ class slip2d():
         GRAVITY = self.gravity
         MASS = self.mass
         RESTING_LENGTH = self.r_length
-        STIFFNESS = self.stiffness
-        #TOTAL_ENERGY
+        self.ENERGY_REQUIRED = 0.5*MASS*pow(self.target_vel,2) - 0.5*MASS*pow(self.state[2],2)
+        self.ENERGY_INJECTED = False
 
-        SPECIFIC_STIFFNESS = STIFFNESS / MASS  # FIXME: Is this name right?
+        self.SPECIFIC_STIFFNESS = self.STIFFNESS / MASS  # FIXME: Is this name right?
 
         # Note: not taken from p[]
         HALF_PI = np.pi / 2.0
@@ -78,9 +79,13 @@ class slip2d():
             # print(energy)
             alpha = np.arctan2(x[1]-x[5], x[0]-x[4]) - HALF_PI
             leg_length = np.sqrt((x[0] - x[4]) ** 2 + (x[1] - x[5]) ** 2)
-            xdotdot = -SPECIFIC_STIFFNESS * (RESTING_LENGTH - leg_length) \
+            if not self.ENERGY_INJECTED and x[3] > 0:
+                self.STIFFNESS = self.STIFFNESS + 2*self.ENERGY_REQUIRED/pow(RESTING_LENGTH - leg_length,2) 
+                self.SPECIFIC_STIFFNESS = self.STIFFNESS / MASS 
+                self.ENERGY_INJECTED = True
+            xdotdot = -self.SPECIFIC_STIFFNESS * (RESTING_LENGTH - leg_length) \
                     * np.sin(alpha)
-            ydotdot = SPECIFIC_STIFFNESS * (RESTING_LENGTH - leg_length) \
+            ydotdot = self.SPECIFIC_STIFFNESS * (RESTING_LENGTH - leg_length) \
                     * np.cos(alpha) - GRAVITY
             return np.array([x[2], x[3], xdotdot, ydotdot, 0, 0])
 
@@ -91,7 +96,7 @@ class slip2d():
             return x[5]
         touchdown_event.terminal = True
 
-        APEX_EVENTS = (fall_event, apex_event)
+        APEX_EVENTS   = (fall_event, apex_event)
         FLIGHT_EVENTS = (fall_event, touchdown_event)
         STANCE_EVENTS = (fall_event, liftoff_event)
 
@@ -137,87 +142,6 @@ class slip2d():
         sol.failed = any(sol.t_events[i].size != 0 for i in (0, 2, 4))
         return sol
 
-    def step_stance(self):
-        '''
-        Solve stance dynamics
-        returns a sol object from integrate.solve_ivp, with all phases
-        '''
-
-        AOA = self.aoa
-        GRAVITY = self.gravity
-        MASS = self.mass
-        RESTING_LENGTH = self.r_length
-        STIFFNESS = self.stiffness
-        #TOTAL_ENERGY
-
-        SPECIFIC_STIFFNESS = STIFFNESS / MASS  # FIXME: Is this name right?
-
-        # Note: not taken from p[]
-        HALF_PI = np.pi / 2.0
-        MAX_TIME = self.max_time
-
-        def resetLegs(x):
-            x[4] = x[0]+np.sin(AOA)*RESTING_LENGTH
-            x[5] = x[1]-np.cos(AOA)*RESTING_LENGTH
-            return x
-        
-        def apex_event(t, x):
-            ''' Event function to reach apex '''
-            return x[3]
-        apex_event.terminal = True
-
-        def fall_event(t, x):
-            ''' Event function to detect the body hitting the floor (failure)
-            '''
-            return x[1]
-        fall_event.terminal = True
-
-        def liftoff_event(t, x, RESTING_LENGTH_SQ=RESTING_LENGTH**2):
-            ''' Event function for maximum spring extension (transition to flight)
-            '''
-            return ((x[0]-x[4])**2 + (x[1]-x[5])**2) - RESTING_LENGTH_SQ
-        liftoff_event.terminal = True
-        liftoff_event.direction = 1
-
-        def stance_dynamics(t, x):
-            # energy = computeTotalEnergy(x,p)
-            # print(energy)
-            alpha = np.arctan2(x[1]-x[5], x[0]-x[4]) - HALF_PI
-            leg_length = np.sqrt((x[0] - x[4]) ** 2 + (x[1] - x[5]) ** 2)
-            xdotdot = -SPECIFIC_STIFFNESS * (RESTING_LENGTH - leg_length) \
-                    * np.sin(alpha)
-            ydotdot = SPECIFIC_STIFFNESS * (RESTING_LENGTH - leg_length) \
-                    * np.cos(alpha) - GRAVITY
-            return np.array([x[2], x[3], xdotdot, ydotdot, 0, 0])
-
-        def touchdown_event(t, x):
-            ''' Event function for foot touchdown (transition to stance)
-            '''
-            # x[1]- np.cos(p["aoa"])*p["resting_length"] (which is = x[5])
-            return x[5]
-        touchdown_event.terminal = True
-
-        STANCE_EVENTS = (fall_event, liftoff_event)
-
-
-        t0 = 0
-        ''' Starting time '''
-        x0 = resetLegs(self.state)        
-        ''' Starting state '''
-
-        # STANCE: simulate till liftoff
-        sol = integrate.solve_ivp(
-                events=STANCE_EVENTS,
-                fun=stance_dynamics,
-                max_step=self.dt,
-                t_span=[t0, t0 + MAX_TIME],
-                y0=x0,
-        )
-
-        # concatenate all solutions
-        sol.failed = (len(sol.t_events) != 0)
-        return sol
-
     def update_state(self, x, y, xdot, ydot):
         self.state      = [ x, y, xdot, ydot, 
                             x+np.sin(self.aoa)*self.r_length, 
@@ -225,3 +149,6 @@ class slip2d():
     
     def set_aoa(self, new_aoa):
         self.aoa = new_aoa
+
+    def set_target_vel(self, new_target_vel):
+        self.target_vel = new_target_vel
